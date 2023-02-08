7b8fb23c46c251124b99574692bba3978496c8c3
SAStevens <118325820+SAStevens@users.noreply.github.com>
2023-02-07 20:42:04 -0700

    FSDI 108 #1

---
+++ data.py
@@ -0,0 +1,13 @@
+me = {
+    "name": "Scott",
+    "last": "Stevens",
+    "age": 88,
+    "hobbies": ["mountain drives", "good food", "long naps"],
+    "address": {
+        "number": 7655,
+        "street": "forest ave.",
+        "city": "springs",
+        "zipcode": "80908"
+    }
+}
+
---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from data import me
+
+import json
+
+app = Flask(__name__) # creating a new instance of the class 'Flask'
+
+
+@app.get("/")
+def home():
+    return "Hello World!"
+
+
+@app.get("/about")
+def about():
+    return "Scott Stevens"
+
+
+@app.get("/contact/me")
+def contact_me():
+    return "saseaa37@gmail.com"
+
+
+
+#########################################################
+################### APT -> JSON #########################
+#########################################################
+
+
+
+@app.get("/api/developer")
+def developer():
+    return json.dumps(me)
+
+
+@app.get("/api/developer/address")
+def dev_address():
+    address = me["address"]
+    # return address["street"] + " #" + str(address["number"]) + ", " + address["city"] + ", " + address["zipcode"]
+    return f'{address["street"]}, #{address["number"]}, {address["city"]}, {address["zipcode"]}'
+    
+
+app.run(debug=True)
